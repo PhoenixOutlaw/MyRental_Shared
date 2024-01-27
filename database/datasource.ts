6a1e5e3446a0554e427576e0de19f4cb9423c570
PhoenixOutlaw <harshyadavv615@gmail.com>
2024-01-27 16:58:56 +0530

    db bug

--- database/datasource.ts
+++ database/datasource.ts
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "test") {
   dotenv.config();
 }
 
-const basePath = __dirname + "../../src/MyRental_Shared/";
+const basePath = __dirname + "../../src/sharedlib/";
 const dataSource: TypeOrmModuleOptions = {
   type: "postgres",
   host: process.env.POSTGRES_HOST,
