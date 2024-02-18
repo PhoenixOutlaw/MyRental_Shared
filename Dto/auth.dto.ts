e9658cc666a382336493030174451dc83c941cb3
Girik Bajaj <girik.bajaj@leverageedu.com>
2024-02-18 14:44:14 +0530

    Add update user dto

--- Dto/auth.dto.ts
+++ Dto/auth.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, Matches, MinLength } from "class-validator";
+import { IsOptional, IsNotEmpty, Matches, MinLength } from "class-validator";
 
 export class Mpin_dto {
   @IsNotEmpty()
@@ -17,3 +17,17 @@ export class CreateUser_dto {
   @Matches(new RegExp(/^[0-9]{10}$/))
   phoneNumber: string;
 }
+
+/**
+ * TODO: Refactor UpdateUser_dto & CreateUser_dto
+ */
+export class UpdateUser_dto {
+  @IsOptional()
+  fullName?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @Matches(new RegExp(/^[2-9]{1}[0-9]{11}$/))
+  aadharNumber?: string;
+}
+
